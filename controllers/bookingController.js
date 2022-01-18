1ca6ad265c5b5a9388401abeccee77ffdc56cc95
sumesh shenoy <sumeshshenoy03@gmail.com>
2022-01-18 20:43:48 +0400

    updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,6 +54,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items.data.object.amount_total / 100;
+  console.log(tour, user, price);
   Booking.create({ tour, user, price });
 };
 
